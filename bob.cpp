0c1f17d33a1925213c7b03da0e4a655b293ea725
Brian H. Ward <bward@virtualhold.com>
2016-08-12 10:41:24 -0400

    All tests passing.

---
+++ bob.cpp
@@ -0,0 +1,25 @@
+// vim: ts=4:sts=4
+
+#include "bob.h"
+#include <regex>
+
+namespace bob
+{
+	std::string hey(const std::string& message)
+	{
+		std::smatch m;
+		static std::regex silence_regex("^\\s*$");
+		static std::regex shouting_regex(".*(!|\\?)$");
+		static std::regex contains_lowercase_regex("[a-z]");
+		static std::regex contains_uppercase_regex("[A-Z]");
+		static std::regex asking_a_question_regex(".*\\?\\s*$");
+		if (std::regex_search(message,m,silence_regex))
+			return "Fine. Be that way!";
+		if (std::regex_search(message,m,contains_uppercase_regex) &&
+		   !std::regex_search(message,m,contains_lowercase_regex))
+			return "Whoa, chill out!";
+		if (std::regex_search(message,m,asking_a_question_regex))
+			return "Sure.";
+		return "Whatever.";
+	}
+}
---
+++ bob.h
@@ -0,0 +1,13 @@
+// vim: ts=4:sts=4
+
+#ifndef __bob_h__
+#define __bob_h__
+
+#include <string>
+
+namespace bob
+{
+	std::string hey(const std::string& message);
+};
+
+#endif
